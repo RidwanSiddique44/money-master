f57c6a49f351fecd8875275510c9836783627af1
RidwanSiddique44 <srsiddique118@gmail.com>
2022-02-17 18:49:49 +0600

    complete js added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
-function getAmount(cost) {
+// ------------ validation Check ------------- //
+function getValue(cost) {
     const amountInput = document.getElementById(cost);
     const amountValue = parseFloat(amountInput.value);
+
     if (isNaN(amountValue)) {
         document.getElementById('for-string').style.display = 'block';
         document.getElementById('for-negetive').style.display = 'none';
+        console.log(click)
+
 
     }
     else if (amountValue < 0) {
         document.getElementById('for-negetive').style.display = 'block';
         document.getElementById('for-string').style.display = 'none';
-
+        console.log(click)
 
 
     }
@@ -20,56 +24,52 @@ function getAmount(cost) {
     }
 
 }
-function totallCount(income, value) {
-    const remaining = income - value;
+// ------------ Remaining calculation ------------- //
+function calculate(bigValue, smallValue) {
+    const remaining = bigValue - smallValue;
     return remaining;
 }
-function input() {
-    const incomeValue = getAmount('income-input');
 
-    const foodAmount = getAmount('food-cost');
+// ------------ Expence & Balance Count ------------- //
+function remain() {
+    const foodAmount = getValue('food-cost');
 
-    const rentAmount = getAmount('rent-cost');
+    const rentAmount = getValue('rent-cost');
 
-    const clothAmount = getAmount('cloth-cost');
-
+    const clothAmount = getValue('cloth-cost');
     const expenses = foodAmount + rentAmount + clothAmount;
     const totallExpenses = document.getElementById('totall-expenses');
     totallExpenses.innerText = expenses;
+    const incomeValue = getValue('income-input');
     if (incomeValue > expenses) {
-        document.getElementById('for-greater').style.display = 'none';
-        const remainingValue = totallCount(incomeValue, expenses);
-
+        document.getElementById('for-logic').style.display = 'none';
+        const remainingValue = calculate(incomeValue, expenses);
         return remainingValue;
     }
+
     else {
-        document.getElementById('for-greater').style.display = 'block';
+        document.getElementById('for-logic').style.display = 'block';
         return 0;
     }
-
-
-
 }
+// ------------ Calculate Button Listener ------------- //
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const balance = document.getElementById('balance');
-
-    balance.innerText = input();
+    balance.innerText = remain();
 })
+// ------------ saving Count & Button Listener ------------- //
 document.getElementById('save-button').addEventListener('click', function () {
-
-    const incomeValue = getAmount('income-input');
-
-    const savingValue = getAmount('saving-input');
-
+    const incomeValue = getValue('income-input');
+    const savingValue = getValue('saving-input');
     const totallSave = incomeValue * savingValue / 100;
     const savingAmount = document.getElementById('saving-amount');
     savingAmount.innerText = totallSave;
-    const prevBalance = input();
-    const remainingBalance = document.getElementById('reamining-balance');
+    const previousBalance = remain();
+    const remainingBalance = document.getElementById('remaining-balance');
 
-    if (prevBalance > totallSave) {
+    if (previousBalance > totallSave) {
         document.getElementById('for-save').style.display = 'none';
-        remainingBalance.innerText = totallCount(prevBalance, totallSave);
+        remainingBalance.innerText = calculate(previousBalance, totallSave);
 
     }
     else {
@@ -78,3 +78,4 @@ document.getElementById('save-button').addEventListener('click', function () {
     }
 
 })
+
